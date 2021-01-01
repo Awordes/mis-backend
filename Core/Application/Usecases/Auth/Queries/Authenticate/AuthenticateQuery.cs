8f109c95f1b59806c086fa5d5eb8fa15e0b44bd7
Andrew Tolstov <awordes76@gmail.com>
2021-01-01 18:04:53 +0300

    Updating.

---
+++ Core/Application/Usecases/Auth/Queries/Authenticate/AuthenticateQuery.cs
@@ -0,0 +1,36 @@
+﻿using Core.Application.Usecases.Auth.ViewModels;
+using Core.Domain.Users;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Application.Usecases.Auth.Queries.Authenticate
+{
+    public class AuthenticateQuery: IRequest<AuthenticationViewModel>
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        private class Handler : IRequestHandler<AuthenticateQuery, AuthenticationViewModel>
+        {
+            private readonly SignInManager<User> _signInManager;
+
+            public Handler(
+                SignInManager<User> signInManager)
+            {
+                _signInManager = signInManager;
+            }
+
+            public async Task<AuthenticationViewModel> Handle(AuthenticateQuery request, CancellationToken cancellationToken)
+            {
+                var result = await _signInManager.PasswordSignInAsync(request.Login, request.Password, request.RememberMe, false);
+                return new AuthenticationViewModel();
+            }
+        }
+    }
+}
--- Presentation/Controllers/AuthController.cs
+++ Presentation/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Core.Application.Usecases.Users.Commands.CreateUser;
+﻿using Core.Application.Usecases.Auth.Queries.Authenticate;
+using Core.Application.Usecases.Auth.ViewModels;
+using Core.Application.Usecases.Users.Commands.CreateUser;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -8,12 +11,21 @@ namespace Presentation.Controllers
     public class AuthController: BaseController
     {
         [HttpPost]
+        [Authorize]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesDefaultResponseType]
         public async Task<IActionResult> CreateUser(CreateUserCommand command)
         {
             await Mediator.Send(command);
             return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<AuthenticationViewModel>> Login([FromQuery] AuthenticateQuery query)
+        {            
+            return await Mediator.Send(query);
         }
     }
 }
